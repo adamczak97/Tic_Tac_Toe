567f36ec33450c3383e9405b08439d9ef15d2287
Łukasz <ukasz@DESKTOP-RTKURN8>
2020-01-19 18:32:24 +0100

    Add project files.

---
+++ Tic_Tac_Toe/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pomocToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("There is no help here");
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void b_click(object sender, EventArgs e)
+        {
+
+            Button b = (Button)sender;
+            int newSize = 75;
+            b.Font = new Font(Button.DefaultFont.FontFamily, newSize);
+            if (turn)
+            {
+                b.Text = "X";
+            }
+            else
+            {
+                b.Text = "O";
+            }
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            check();
+        }
+
+
+
+        private void check()
+        {
+            bool winner = false;
+
+            //horizontal
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                winner = true;
+
+            // vertical
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                winner = true;
+
+
+            //diagonal
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+                winner = true;
+
+
+            if (winner)
+            {
+                disableButtons();
+                string who_win = "";
+                if (turn)
+                    who_win = "O";
+                else
+                    who_win = "X";
+
+                MessageBox.Show(who_win + " wins!");
+            }
+            else
+            {
+                if (turn_count == 9)
+                    MessageBox.Show("Draw!");
+            }         
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+
+        }
+
+        private void nowaGraToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
+
