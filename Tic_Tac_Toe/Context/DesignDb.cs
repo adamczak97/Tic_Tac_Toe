dd26d2a82fbe2d5842e3def5c82638183ce45888
adamczakl <aadamczak@edu.cdv.pl>
2020-01-21 17:57:38 +0100

    17:57

---
+++ Tic_Tac_Toe/Context/DesignDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Tic_Tac_Toe.Context
+{ 
+    public class DesignDb : IDesignTimeDbContextFactory<ScoreDbEntities>
+    {
+     public ScoreDbEntities CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ScoreDbEntities>();
+            var connectionString = @"Dat Source=cars.db;";
+            builder.UseSqlite(connectionString);
+            return new ScoreDbEntities(builder.Options);
+        }
+   }
+}
--- Tic_Tac_Toe/Repository.cs
+++ Tic_Tac_Toe/Repository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tic_Tac_Toe.App.Entities
+using Tic_Tac_Toe.App.Entities;
 
 namespace Tic_Tac_Toe.App
 {
